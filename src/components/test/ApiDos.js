7a4cd1a7e463ce5bdcfbe43bb38141da5894fa00
nacho <nacho.mota@gmail.com>
2020-08-24 23:20:51 +0200

    Fech Api OK async

--- src/components/test/ApiDos.js
+++ src/components/test/ApiDos.js
@@ -1,13 +1,25 @@
 import React, { useState, useEffect } from 'react';
 
+
+
 export default function AsyncHooks() {
   const [search, setSearch] = useState('')
   const [query, setQuery] = useState('');
 
 
   useEffect(() => {
-    console.log({ query })
-  },[query]);
+    async function fetchData() {
+      try {
+        const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=gvYWMneH5JY1E3kCJsXsWn9VkoCtRcMQ&q=${query}&limit=10&offset=0&rating=r&lang=en
+    `);
+        const json = await response.json();
+        console.log({ json })
+      } catch (error) { }
+    }
+    if (query !== '') {
+      fetchData();
+    }
+  }, [query]);
 
 
 

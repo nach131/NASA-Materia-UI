fc998b3b2145353feb132790140f133d71f85ddc
nacho <nacho.mota@gmail.com>
2020-08-24 23:06:15 +0200

    funcion submit puesta en el form

--- src/components/test/ApiDos.js
+++ src/components/test/ApiDos.js
@@ -1,31 +1,30 @@
-import React, { useState, useEffect}  from 'react';
+import React, { useState, useEffect } from 'react';
 
-export default function AsyncHooks(){
+export default function AsyncHooks() {
   const [search, setSearch] = useState('')
   const [query, setQuery] = useState('');
 
-  function onSubmit(e) {
-    e.preventDefault();
-    setQuery(search);
-    // console.log(search)
-  }
-useEffect(() => {
-console.log({query})
-});
+
+  useEffect(() => {
+    console.log({ query })
+  },[query]);
 
 
 
-return (
-  <div>
-    <h1>AsyncHooks</h1>
-    <form onSubmit={onSubmit}>
-      <input 
-      value={search}
-      onChange={e => setSearch(e.target.value)}
-      placeholde="Vuscar Gif"
-      type="text"/>
-      <button type="submit">Search</button>
-    </form>
-  </div>
-)
+  return (
+    <div>
+      <h1>AsyncHooks</h1>
+      <form onSubmit={e => {
+        e.preventDefault();
+        setQuery(search);
+      }}>
+        <input
+          value={search}
+          onChange={e => setSearch(e.target.value)}
+          placeholde="Vuscar Gif"
+          type="text" />
+        <button type="submit">Search</button>
+      </form>
+    </div>
+  )
 }

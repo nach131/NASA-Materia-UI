5a934a9fbd393d932a740facce3d426c2c47619a
nacho <nacho.mota@gmail.com>
2020-08-24 20:07:40 +0200

    Hook a API solo el nombre

--- src/components/Api.js
+++ src/components/Api.js
@@ -2,11 +2,15 @@ import React, { useState, useEffect } from 'react';
 
 
 export default () => {
-  const [contar, setContar] = useState(0)
+  const [contar, setContar] = useState(0);
+  const [usuario, setUsuario] = useState(null);
 
   //similar a componentDidMount y componentDidUpdate
   useEffect(async () => {
-    const respuesta = await fetch();
+    const respuesta = await fetch('https://api.randomuser.me/');
+    const datos = await respuesta.json();
+    const [item] = datos.results
+    setUsuario(item);
   }, []);
 
 
@@ -16,6 +20,7 @@ export default () => {
         Tu click {contar} veces
     </p>
       <button onClick={() => setContar(contar + 1)}>Click</button>
+      {usuario && <div>{usuario.name.first}</div>}
     </div>
   );
 
